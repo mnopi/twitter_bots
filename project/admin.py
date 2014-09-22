a0bd2ec637e5271f2d6f66bf33213359b8dbc2e4
Michel Barbou Salvador <michel@bizeulabs.com>
2014-09-22 14:53:26 +0200

    modelos en project agregados a admin de django

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from project.models import *
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(TweetMsg)
+admin.site.register(TargetUser)
+admin.site.register(Follower)
+admin.site.register(TwitterUser)
+admin.site.register(Tweet)
--- project/models.py
+++ project/models.py
@@ -5,21 +5,33 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=100, null=False, blank=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class TweetMsg(models.Model):
     text = models.CharField(max_length=160, null=False, blank=True)
     project = models.ForeignKey(Project, related_name='tweet_msgs')
+
+    def __unicode__(self):
+        return '%s @ %s' % (self.text, self.project.name)
 
 
 class TargetUser(models.Model):
     username = models.CharField(max_length=80, null=False, blank=True)
     projects = models.ManyToManyField(Project, related_name='target_users')
 
+    def __unicode__(self):
+        return self.username
+
 
 class Follower(models.Model):
-    target_user = models.ForeignKey(TargetUser, related_name='followers')
+    target_user = models.ForeignKey(TargetUser, related_name='followers', null=False)
     # follower = models.ForeignKey('project.models.TwitterUser', related_name='twitter_user')
-    twitter_user = models.OneToOneField('TwitterUser', related_name='follower')
+    twitter_user = models.OneToOneField('TwitterUser', related_name='follower', null=False)
+
+    def __unicode__(self):
+        return '%s -> %s' % (self.twitter_user, self.target_user)
 
 
 class TwitterUser(models.Model):
@@ -29,16 +41,22 @@ class TwitterUser(models.Model):
     username = models.CharField(max_length=160, null=False, blank=True)
     twitter_id = models.BigIntegerField(null=False, blank=True)
     country = models.CharField(max_length=2, null=True, blank=True)
-    language = models.CharField(max_length=2, null=True, blank=True)
+    language = models.CharField(max_length=2, null=False, blank=True, default='en')
     city = models.CharField(max_length=80, null=True, blank=True)
     created_date = models.DateTimeField(null=False, blank=True)
 
+    def __unicode__(self):
+        return self.username
+
 
 class Tweet(models.Model):
-    text = models.CharField(max_length=160, null=False)
+    tweet_msg = models.ForeignKey(TweetMsg, null=False)
     date = models.DateTimeField(null=False)
     project = models.ForeignKey(Project, related_name="tweets")
     mentioned_users = models.ManyToManyField(TwitterUser, related_name='mentions')
+
+    def __unicode__(self):
+        return self.tweet_msg
 
 
 
